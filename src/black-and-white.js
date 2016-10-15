19e8bbc8eb6bea1de1e04443dc74688ede60d961
Silent K <silentksionda@gmail.com>
2016-10-14 22:52:22 -0700

    Static black and white setup.

---
+++ src/black-and-white.js
@@ -0,0 +1,36 @@
+'use strict';
+
+Crafty.scene('black-and-white', function () {
+    var height = Crafty.viewport.height,
+        width = Crafty.viewport.width;
+
+    var player;
+
+    Crafty.background('white');
+    Crafty.e('Color,2D,DOM')
+        .color('black')
+        .attr({x: width/6, y:0, w: width/6, h: height})
+    ;
+
+    Crafty.e('Color,2D,DOM')
+        .color('black')
+        .attr({x: width/2, y:0, w: width/6, h: height})
+    ;
+
+    Crafty.e('Color,2D,DOM')
+        .color('black')
+        .attr({x: width*5/6, y:0, w: width/6, h: height})
+    ;
+
+    platformConstructor(width/4, height/2, width/12, 25, '#FFFFFF');
+    platformConstructor(width/3, height/2, width/6, 25, '#000000');
+    platformConstructor(width/2, height/2, width/6, 25, '#FFFFFF');
+    platformConstructor(width*2/3, height/2, width/12, 25, '#000000');
+
+    platformConstructor(0, height, width, 25, '#000000');
+    wallConstructor(-100, 0, 100, height, '#000000');
+    wallConstructor(width + 1, 0, 100, height, '#000000');
+
+    player = playerConstructor(125, height - 25, 'black');
+});
+
--- src/game.js
+++ src/game.js
@@ -1,11 +1,6 @@
 'use strict';
 
 playerComponent();
-startButtonComponent();
 
-Crafty.scene('menu', {
-    x: 100,
-    y: Crafty.viewport.height - 100,
-    color: () => 'red'
-});
+Crafty.scene('black-and-white');
 
